2b91db7617bfd6ed9105fbce9cfa51af0804a5fb
Luiz Araujo <arauldesign@gmail.com>
2022-05-03 19:52:08 -0300

    subindo apps de console

---
+++ CalculatorNet/CalculatorNet/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace CalculatorNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            
+        }
+
+        static void Menu()
+        {   
+            Console.Clear();
+            Console.WriteLine("CALCULADORA!");
+            Console.WriteLine("====================");
+            Console.WriteLine("1 - SOMA");
+            Console.WriteLine("2 - SUBTRAÇÃO");
+            Console.WriteLine("3 - DIVISÃO");
+            Console.WriteLine("4 - MULTIPLICAÇÃO");
+            Console.WriteLine("5 - PORCENTAGEM");
+            Console.WriteLine("6 - SAIR");
+            Console.WriteLine("====================");
+            Console.WriteLine("Escolha uma opção: "); 
+            
+            short resp = short.Parse(Console.ReadLine());
+
+            switch (resp)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: Porcentagem(); break;
+                case 6: System.Environment.Exit(0); break;
+                default: Menu(); break;
+        
+            }
+        }
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Valor 1:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 2:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine($"O valor da soma é {v1 + v2}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Valor 1:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 2:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine($"O valor da subtração é {v1 - v2}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Divisao()
+        {
+            Console.WriteLine("Valor 1:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 2:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine($"O valor da divisão é {v1 / v2}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.WriteLine("Valor 1:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 2:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine($"O valor da multiplicação é {v1 * v2}");
+            Console.ReadKey();
+            Menu();
+
+        }
+
+        static void Porcentagem()
+        {
+            Console.WriteLine("Valor 1:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Quanto porcento do valor 1:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float perc = v1 * v2 / 100;
+            Console.WriteLine($"{v2}% de {v1} é {perc}");
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,95 @@
+﻿ using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            
+
+            Console.WriteLine("Quando tempo deseja contar? Ex: 25s, 2m");
+
+            string data = Console.ReadLine().ToLower();
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            char type = char.Parse(data.Substring(data.Length - 1));
+
+            int multi = 1;
+
+            if (type == 'm')
+                multi = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            Console.WriteLine("===============");
+            Console.WriteLine("1 - PROGRESSIVO");
+            Console.WriteLine("2 - REGRESSIVO");
+            Console.WriteLine("0 - SAIR);
+            Console.WriteLine("===============");
+            Console.WriteLine("Escolha uma opção: ");
+            int tipo = int.Parse(Console.ReadLine());
+
+            switch (tipo)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: PreStart(); StartProgressivo(time * multi); break;
+                case 2: PreStart(); StartRegressivo(time * multi); break;
+                default: Menu(); break;
+            }
+            
+
+        }
+        static void PreStart()
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(1000);
+        }
+        static void StartProgressivo(int time)
+        {
+            int currentTime = 0;
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Cronômetro finalizado!");
+            Thread.Sleep(2500);
+            Menu();
+        }
+
+        static void StartRegressivo(int time)
+        {
+            int currentTime = time;
+            while (currentTime > 0)
+            {
+                Console.Clear();
+                Console.WriteLine(currentTime);
+                currentTime--;
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Cronômetro finalizado!");
+            Thread.Sleep(2500);
+            Menu();
+        }
+       
+
+    }
+}
