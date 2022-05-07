bf8009826b8153527e28313f5964524d998ccaac
Luiz Araujo <arauldesign@gmail.com>
2022-05-07 11:38:57 -0300

    Writing lines options

--- EditorHTML/EditorHTML/Menu.cs
+++ EditorHTML/EditorHTML/Menu.cs
@@ -13,7 +13,7 @@ namespace EditorHTML
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Green;       
-            DrawScreen(100, 40);
+            DrawScreen(30, 10);
         }
 
         public static void DrawScreen(int colunas, int linhas)
@@ -42,7 +42,29 @@ namespace EditorHTML
                     Console.Write(" ");
                 Console.Write("|");
                 Console.Write("\n");
+
+                var option = short.Parse(Console.ReadLine());
             }
+
+            WriteOptions();
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("==============");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo: ");
+            Console.SetCursorPosition(3, 5);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 9);
+            Console.Write("Opção: ");
         }
     }
 }
